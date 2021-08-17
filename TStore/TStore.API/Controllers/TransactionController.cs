f7512c25e679a8c201dc3a34895c2daf42654df4
creatorQuen <spastime@yandex.ru>
2021-08-17 16:21:28 +0300

    Add TransactionController, add swagger and nuget libraries

---
+++ TStore/TStore.API/Controllers/TransactionController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TStore.API.Models;
+using TStore.Business.Services;
+using TStore.DAL.Models;
+
+namespace TStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TransactionController : Controller
+    {
+        private readonly ITransactionService _transactionService;
+        private readonly IMapper _mapper;
+
+        public TransactionController(ITransactionService transactionService, IMapper mapper)
+        {
+            _transactionService = transactionService;
+            _mapper = mapper;
+        }
+
+        // api/transaction
+        [HttpPost("transaction")]
+        [Description("Add transaction")]
+        public TransactionOutputModel AddTransaction([FromBody] TransactionInputModel inputModel)
+        {
+            var dto = _mapper.Map<TransactionDto>(inputModel);
+            var returnedDto = _transactionService.AddTransaction(dto);
+            var output = _mapper.Map<TransactionOutputModel>(returnedDto);
+
+            return output;
+        }
+
+        // api/transaction/get-all-transactions
+        [HttpPost("transaction/get-all-transactions")]
+        [Description("Get all transactions")]
+        public List<TransactionOutputModel> GetAllTransactions()
+        {
+            var resultDto = _transactionService.GetAllTransaction();
+            var listOutputs = _mapper.Map<List<TransactionOutputModel>>(resultDto);
+
+            return listOutputs;
+        }
+
+    }
+}
--- TStore/TStore.API/Startup.cs
+++ TStore/TStore.API/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using TStore.Business.Services;
+using TStore.DAL.Repositories;
 
 namespace TStore.API
 {
@@ -19,12 +21,27 @@ namespace TStore.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddScoped<ITransactionRepository, TransactionRepository>();
+            services.AddScoped<ITransactionService, TransactionService>();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TStore.API", Version = "v1" });
+            //});
+
+            services.AddSwaggerDocument(document =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TStore.API", Version = "v1" });
+                document.DocumentName = "Endpoints for TStore";
+                document.Title = "TStore API";
+                document.Version = "v8";
+                document.Description = "An interface for TStore.";
+
             });
+
+            // Add framework services. 
+            services.AddOptions();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +50,17 @@ namespace TStore.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TStore.API v1"));
+                app.UseOpenApi();
+                app.UseSwaggerUi3();
             }
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
