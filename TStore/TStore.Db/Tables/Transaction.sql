bfeceb72dd089acfafdaf8b8c937803faac6d0b6
Vadim <vki-13@yandex.ru>
2021-08-17 13:09:08 +0300

    Created projects, import Db

---
+++ TStore/TStore.Db/Tables/Transaction.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Transaction] (
+    [Id]              INT               NOT NULL IDENTITY (1, 1),
+    [AccountId]       INT               NOT NULL,
+    [TransactionType] INT               NOT NULL,
+    [Date]            DATETIME2 (7)     NOT NULL,
+    [Amount]          DECIMAL (14,3)    NOT NULL,
+    CONSTRAINT [PK_TRANSACTION] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
