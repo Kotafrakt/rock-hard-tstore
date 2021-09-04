99c8bcfb229d1d9acdf597207cf0fb198fddf854
Vadim Sandutsa <vadimsanduca@gmail.com>
2021-09-04 06:50:02 +0300

    ChangeSelectByPeriodStoredProcedure

--- TStore/TStore.Db/StoredProcedures/Transaction/Transaction_SelectByPeriod.sql
+++ TStore/TStore.Db/StoredProcedures/Transaction/Transaction_SelectByPeriod.sql
@@ -4,6 +4,7 @@
 	@AccountId			int = NULL
 AS
 BEGIN
+IF @AccountId is null
 	SELECT
 		t.Id,		
 		t.AccountId,
@@ -12,7 +13,17 @@ BEGIN
 		t.TransactionType,
 		t.[Date]
 	FROM [dbo].[Transaction] t
-	WHERE t.[Date] BETWEEN @From and @To and t.AccountId = @AccountId or @AccountId IS NULL
+	WHERE t.[Date] BETWEEN @From and @To
+ELSE
+	SELECT
+		t.Id,		
+		t.AccountId,
+		t.Amount,
+		t.Currency,
+		t.TransactionType,
+		t.[Date]
+	FROM [dbo].[Transaction] t
+	WHERE t.[Date] BETWEEN @From and @To and t.AccountId = @AccountId
 UNION ALL
 	SELECT
 		tr.Id,
@@ -24,6 +35,6 @@ UNION ALL
 	FROM [dbo].[Transaction] t
 	LEFT JOIN [dbo].[Transaction] tr
 	ON t.Date = tr.Date
-	WHERE t.[Date] BETWEEN @From and @To and t.TransactionType = 3 and t.AccountId = @AccountId and tr.AccountId <> t.AccountId or @AccountId IS NULL
+	WHERE t.[Date] BETWEEN @From and @To and t.AccountId = @AccountId and tr.AccountId <> t.AccountId and t.TransactionType = 3 
 	ORDER BY t.Date
 END
